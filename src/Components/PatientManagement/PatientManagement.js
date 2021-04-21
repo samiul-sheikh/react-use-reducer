9e4aa65c3da39fb3ad3c2df461f4e415c9c99edb
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-04-21 22:55:45 +0600

    useReducer with action and payload

--- src/Components/PatientManagement/PatientManagement.js
+++ src/Components/PatientManagement/PatientManagement.js
@@ -1,12 +1,34 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useRef } from 'react';
 import { patientReducer, patientState } from '../reducers/patientReducer';
 
 const PatientManagement = () => {
+
+    const nameRef = useRef();
     const [state, dispatch] = useReducer(patientReducer, patientState);
-    console.log(state);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        dispatch({
+            type: 'ADD_PATIENT',
+            name: nameRef.current.value,
+            id: state.patients.length + 1
+        })
+        nameRef.current.value = '';
+        // console.log(nameRef.current.value);
+    }
+
     return (
         <div>
             <h2>Patient Management: {state.patients.length}</h2>
+            <form onSubmit={handleSubmit}>
+                <input ref={nameRef} />
+            </form>
+            {
+                state.patients.map(pt => <li
+                    key={pt.id}
+                    onClick={() => dispatch({ type: 'REMOVE_PATIENT', id: pt.id })}
+                >{pt.name}</li>)
+            }
         </div>
     );
 };
--- src/Components/ReduceCount/ReduceCount.js
+++ src/Components/ReduceCount/ReduceCount.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 
 const initialState = { count: 0 };
 
@@ -16,7 +16,6 @@ const reducer = (state, action) => {
 
 const ReduceCount = () => {
 
-    // const [count, setCount] = useState(0);
     const [state, dispatch] = useReducer(reducer, initialState);
 
     return (
